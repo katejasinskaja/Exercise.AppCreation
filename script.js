4d646f417501b443c88eb614f82a59f3755a814b
Kate Yasinskaya <katherine_yasinskaya@mail.ru>
2023-09-17 13:09:56 +0300

    Apply conditions and cycles

--- script.js
+++ script.js
@@ -12,8 +12,34 @@ const personalMovieDB = {
 
     
 };
-const theLastFilmWatched = prompt("Один из последних просмотренных фильмов?","");
-const estimationOfTheFilm = prompt("На сколько оцените его?", "");
-personalMovieDB.movies[theLastFilmWatched] = estimationOfTheFilm;
+
+for (let i = 0; i < 2; i++) {
+    const theLastFilmWatched = prompt("Один из последних просмотренных фильмов?","");
+    const estimationOfTheFilm = prompt("На сколько оцените его?", "");
+    
+    if ((theLastFilmWatched !== null) && (estimationOfTheFilm !== null) &&
+        (theLastFilmWatched !== "") && (estimationOfTheFilm !== "") &&
+        (theLastFilmWatched.length < 50)) {
+        personalMovieDB.movies[theLastFilmWatched] = estimationOfTheFilm;
+        console.log("done");
+    } else {
+        console.log("error");
+        i--;
+    }
+
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
+
+
+
